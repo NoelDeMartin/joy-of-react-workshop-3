c04d9f9009605c9c1ee5c5732ec704289486bdf8
Noel De Martin <noel@noeldemartin.com>
2026-02-24 08:39:53 +0100

    Exercise 3: Adding metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,6 +6,16 @@ import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 import { loadBlogPost } from '@/helpers/file-helpers';
 
+export async function generateMetadata({ params }) {
+    const { postSlug } = await params;
+    const post = await loadBlogPost(postSlug);
+
+    return {
+        title: post.frontmatter.title,
+        description: post.frontmatter.abstract,
+    }
+}
+
 async function BlogPost({ params }) {
     const { postSlug } = await params;
     const post = await loadBlogPost(postSlug);
